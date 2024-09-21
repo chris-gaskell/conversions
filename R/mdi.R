6084b97538f98757773435470603cf2f86b5ca35
chris-gaskell <chris-gaskell@hotmail.co.uk>
2024-09-21 18:57:44 +0100

    add mdi

--- R/mdi.R
+++ R/mdi.R
@@ -11,7 +11,8 @@
 #' @param dp Number of decimal places for rounding in the results. Default is 7.
 #'
 #' @return A list containing:
-#' \item{mdi}{The Mahalanobis Distance Index.}
+#' \item{mdi_value}{The Mahalanobis Distance Index.}
+#' \item{df}{Degrees of freedom used in the calculation.}
 #' \item{p_value}{The p-value associated with the MDI.}
 #' \item{prev}{The percentage of the normative population expected to exhibit a more unusual profile.}
 #'
@@ -27,8 +28,11 @@
 #' mdi_results <- mdi(R, x, norm.m = norm.m, norm.sd = norm.sd)
 #' print(mdi_results)
 mdi <- function(R, x, norm.m, norm.sd, dp = 7) {
-  scalar_quant <- norm.sd * norm.sd
-  sigma <- scalar_quant * R # covariance matrix (Σ)
+  if (!is.matrix(R) || ncol(R) != length(x)) stop("R must be a square matrix with dimensions matching the length of the test scores vector")
+  if (length(x) != length(norm.m) || length(x) != length(norm.sd)) stop("x, norm.m, and norm.sd must have the same length")
+
+  scalar_quant <- norm.sd^2
+  sigma <- diag(scalar_quant) %*% R # covariance matrix (Σ)
   sigma_inv <- solve(sigma) # inverse of the covariance matrix (Σ⁻¹)
   diff <- x - norm.m
   mdi_value <- as.numeric(t(diff) %*% sigma_inv %*% diff)
@@ -40,7 +44,7 @@ mdi <- function(R, x, norm.m, norm.sd, dp = 7) {
   result <- list(
     mdi_value = round(mdi_value, 3),
     df = df,
-    p_value = round(p_value, 7),
+    p_value = round(p_value, dp),
     prev = round(prev, 4)
   )
 
@@ -49,24 +53,14 @@ mdi <- function(R, x, norm.m, norm.sd, dp = 7) {
 }
 
 
-#' Custom Print Method for MDI
-#'
-#' This function provides a custom print method for displaying the results
-#' of the Mahalanobis Distance Index (MDI) calculation in a formatted way.
-#'
-#' @param x An object containing the MDI results, which includes the
-#' chi-square value, degrees of freedom (df), and p-value.
-#' @param ... Additional arguments passed to other methods (currently not used).
-#'
-#' @return The function prints the formatted MDI result but does not return a value.
 #' @export
 print.mdi <- function(x, ...) {
   # Print the results
-  name <- "MAHALANOBIS DISTANCE Index of the overall abnormality of the case's Index score profile: "
-  mdi_str <- glue::glue("Chi-square = {x} on {x$df} df, p value = {x$p_value}")
+  name <- "MAHALANOBIS DISTANCE Index of the overall abnormality of the case's Index score profile:"
+  mdi_str <- glue::glue("Chi-square = {x$mdi_value} on {x$df} df, p value = {x$p_value}")
   p_value_str <- glue::glue("Percentage of normative population expected to exhibit a more unusual profile = {x$prev}%")
 
   # Combine everything into the final result string
   result <- paste(name, mdi_str, p_value_str, sep = "\n\n")
-  cat(result)
+  cat(result, "\n")
 }
